310a6c8c269bb74e9c00fea0331c2c35488642d3
Nicholas Sutin <nick@MBA.local>
2026-01-21 11:59:37 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <div>
+            <h1>page</h1>
+            <div className="bg-amber-300 w-30 h-30"></div>
+        </div>
+    );
+}
